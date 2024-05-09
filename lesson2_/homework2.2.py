533b504d7b959788441bcf65da74e880855ad94c
alexjae1917 <alex777544@gmail.com>
2024-05-09 15:13:43 +0300

    homework commit

--- lesson2_/homework2.2.py
+++ lesson2_/homework2.2.py
@@ -1,7 +1,8 @@
 number = int(input("Enter 5digit number ", ))
-rev = number % 10
-rev = rev * 10 + (number % 100) // 10
-rev = rev * 10 + (number % 1000) // 100
-rev = rev * 10 + (number % 10000) // 1000
-rev = rev * 10 + (number % 100000) // 10000
+n5 = number % 10
+n4 = (number % 100) // 10
+n3 = (number % 1000) // 100
+n2 = (number % 10000) // 1000
+n1 = (number % 100000) // 10000
+rev = (((n5*10 + n4) * 10 + n3) * 10 + n2) * 10 + n1
 print(rev)
