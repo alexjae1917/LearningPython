1b578d513f52139799555306d7d42b61e6807778
alexjae1917 <alex777544@gmail.com>
2024-05-13 13:46:14 +0300

    homework commit

--- lesson3_/homework3.3.py
+++ lesson3_/homework3.3.py
@@ -4,24 +4,19 @@ ex3 = [1, 2, 3, 4, 5]
 ex4 = [1]
 ex5 = []
 
-ex = ex3
+ex = ex4
 
 res = []
 j = len(ex) // 2
 
-if len(ex) == 1:
-    res.append(ex[0])
-    res.append([])
-
-elif len(ex) % 2 == 0:
+if  len(ex) % 2 == 0:
     res.append(ex[:j])
     res.append(ex[j:])
 
-elif len(ex) % 2 != 0:
+else:
     res.append(ex[:j + 1])
     res.append(ex[j + 1:])
 
-else:
-    res = [[], []]
+
 
 print(res)
