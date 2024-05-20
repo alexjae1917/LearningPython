fc7445cc691ccc1c3834bfab0263e516af7626ee
alexjae1917 <alex777544@gmail.com>
2024-05-20 13:18:34 +0300

    setup commit

---
+++ lesson4_/homework4.2.py
@@ -0,0 +1,12 @@
+exm1 = [1,3,5]
+exm2 = [0]
+exm3= [6]
+exm4 = [9, 0, 7, 31, 0, 45, 0, 45, 0, 0, 96, 0]
+
+exm = exm2
+sum = 0
+
+for i in range(0,len(exm),2):
+     sum += exm[i]
+
+print(sum * exm[-1])
--- lesson4_/homework4.3.py
+++ lesson4_/homework4.3.py
@@ -1,13 +1,7 @@
+import random
 
-ex1 = [0, 1, 0, 12, 3]
-ex2 = [0]
-ex3 = [1, 0, 13, 0, 0, 0, 5]
-ex4 = [9, 0, 7, 31, 0, 45, 0, 45, 0, 0, 96, 0]
-
-ex = ex4
-
-for i in range(0,len(ex)):
-    if ex[i] == 0:
-         ex.append(ex.pop(i))
-
-print(ex)
+lst =[]
+for i in range(0,random.randint(3,10)):
+    lst.append(random.randint(0,10))
+new_lst=[lst[0],lst[2],lst[-2]]
+print(lst,'->->',new_lst)
