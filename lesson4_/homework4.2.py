b5cc7c92db3904712d7b3dc88af9c10504a2e17e
alexjae1917 <alex777544@gmail.com>
2024-05-20 17:37:15 +0300

    setup commit

--- lesson4_/homework4.2.py
+++ lesson4_/homework4.2.py
@@ -3,11 +3,11 @@ exm2 = [0]
 exm3= [6]
 exm4 = [9, 0, 7, 31, 0, 45, 0, 45, 0, 0, 96, 0]
 
-exm = exm2
+exm = exm1
 sum = 0
 
-for i in range(0,len(exm),2):
-     sum += exm[i]
+for i in exm[::2]:
+     sum += i
 
 print(sum * exm[-1])
 
