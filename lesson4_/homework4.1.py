9d350706e389d870d5c36cbcb94ffabfddfd3db7
alexjae1917 <alex777544@gmail.com>
2024-05-20 17:35:18 +0300

    setup commit

--- lesson4_/homework4.1.py
+++ lesson4_/homework4.1.py
@@ -1,18 +1,26 @@
-
 ex1 = [0, 1, 0, 12, 3]
 ex2 = [0]
 ex3 = [1, 0, 13, 0, 0, 0, 5]
 ex4 = [9, 0, 7, 31, 0, 45, 0, 45, 0, 0, 96, 0]
 
 ex = ex4
-ex.reverse()
-res = []
-for i in ex:
-    if i == 0:
-        res.append(i)
-    else:
-        res.insert(0,i)
 
 
+def zero(n):
+    return n==0
 
-print(res)
+
+ex.sort(key=zero)
+print(ex)
+
+# ex.reverse()
+# res = []
+# for i in ex:
+#     if i == 0:
+#         res.append(i)
+#     else:
+#         res.insert(0,i)
+#
+#
+#
+# print(res)
