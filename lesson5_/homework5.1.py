1051b69c8469c6364b0fa939371add07e237170b
alexjae1917 <alex777544@gmail.com>
2024-05-23 17:39:08 +0300

    homework 5 commit

--- lesson5_/homework5.1.py
+++ lesson5_/homework5.1.py
@@ -1,15 +1,37 @@
 import keyword
 import string
 
-name = input('Enter new string: ')
-cnt=0
-for i in name:
-    if not i.isalpha():
-        cnt+=1
+
+
+def check(name):
+    cnt=0
+
+    if not name[0].isalpha() and name[0] !="_":
+        return False
+    else:cnt=1
+
+    if name in keyword.kwlist:
+        return False
+
+    for i in name[1:-1]:
+
+        if i.isalpha():
+            cnt += 1
+            if i != i.upper():return False
+
+        if i in string.punctuation and i != '_' or i ==' ':
+            return False
+
+    if cnt == 0: return False
+
+    return True
+m=0
+while m<15:
+    name = input('Enter new string: ')
+    print(check(name))
+    m+=1
 
 
 
-
-
-print('_' in string.punctuation)
+#print('_' in string.punctuation)
 
