ea25628f4a318d2d2131b323385809308eb13cae
alexjae1917 <alex777544@gmail.com>
2024-06-07 17:41:34 +0300

    homework 6

--- lesson9_/homework9.5.py
+++ lesson9_/homework9.5.py
@@ -1,1 +1,20 @@
-.
+import time
+
+
+def my_decorator(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args,**kwargs)
+        end = time.time() - start
+        return end
+    return wrapper
+
+
+@ my_decorator
+def test_func(n: int):
+    cnt = 1
+    while cnt < n:
+        cnt += 1
+
+
+print(test_func(100000000))
