173be46aaaaf85b848bd0d7f20ea4d55fb894c22
alexjae1917 <alex777544@gmail.com>
2024-06-07 17:50:30 +0300

    homework 6

---
+++ lesson9_/homework9.6.py
@@ -0,0 +1,20 @@
+from typing import Callable
+
+
+def my_decorator(func: Callable):
+    def wrapper(*args, **kwargs):
+        name = func.__name__
+        result = func(*args, **kwargs)
+        return f'Function name {name}, with position arguments {args}, named arguments {kwargs}, and result {result}'
+
+    return wrapper
+
+
+@my_decorator
+def test_func(n: int, cnt=5):
+    while cnt < n:
+        cnt *= cnt
+
+    return cnt
+
+print(test_func(100, cnt=10))
