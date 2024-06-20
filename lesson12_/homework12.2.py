4da4bd70c19f3112a135bbafbb531e2f754da98d
alexjae1917 <alex777544@gmail.com>
2024-06-20 16:03:37 +0300

    homework commit

---
+++ lesson12_/homework12.2.py
@@ -0,0 +1,63 @@
+class Product():
+
+    def __init__(self,name: str, price: int,type: str,country: str):
+        self.name = name
+        self.price = price
+        self.type = type
+        self.country = country
+
+    def __str__(self):
+        return f'{self.name} price: {self.price}'
+
+
+class Purchaser():
+
+    def __init__(self, first_name: str, last_name: str, patronymic: str, phone_number: int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.patronymic = patronymic
+        self.phone_number = phone_number
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}, phone number: {self.phone_number}'
+
+
+class Order():
+
+    def __init__(self, purchaser: Purchaser):
+        self.purchaser = purchaser
+        self.cart = {}
+        self.total = 0
+
+    def __str__(self):
+        return f'{self.purchaser}\n{dict(self.cart)}'
+
+    def add_item(self, item: Product, count: int):
+        self.cart[item] = count
+
+    def del_item(self, item: Product):
+        for product,cnt in self.cart.items():
+            if product == item:
+                self.cart.pop(product)
+
+    def get_total(self):
+        self.total = 0
+        for product, cnt in self.cart.items():
+            self.total += (self.product.price * cnt)
+        return self.total
+
+
+user1 = Purchaser('Vasiliy','Vasiliev','Ivanovich',380975641020)
+user2 = Purchaser('Ivan','Fedorov','Petrovich',380872649011)
+user3 = Purchaser('Alexander','Makarov','Alexeevich',3809645342)
+
+Zebra = Product('Zebra DS', 2500, 'scaner', 'USA')
+Xprinter = Product('XPC 58 H', 3500, 'printer','China')
+HPC = Product ('HPC 16s', 2100, 'cashbox', 'Lithuania')
+
+cart1 = Order(user1)
+cart2 = Order(user2)
+cart3 = Order(user3)
+
+cart1.add_item()
+
