240b016fcb17c68b11fa511a075510b1300318e3
alexjae1917 <alex777544@gmail.com>
2024-06-22 00:31:58 +0300

    homework 6

--- lesson6_/homework6.2.py
+++ lesson6_/homework6.2.py
@@ -6,10 +6,10 @@ if 8640000 > seconds > 0:
     minutes = seconds % 3600 // 60
     seconds = seconds % 3600 % 60
 
-    if day == 1:
+    if day % 10 == 1 and day != 11:
         dayprint = 'день'
 
-    elif day > 1 and day < 5:
+    elif 1 < (day % 10) < 5 and  day not in range(11,15):
         dayprint = 'дня'
 
     print(f'{day} {dayprint} {hours:02}:{minutes:02}:{seconds:02}')
